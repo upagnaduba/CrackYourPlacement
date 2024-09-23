808f455845ddd089ddfd111325452435b6676755
upagnaduba <151008060+upagnaduba@users.noreply.github.com>
2024-09-23 11:02:17 +0530

    Time: 45 ms (17.01%), Space: 11.7 MB (58.78%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        freq={}
+        for x in nums1:
+            if x not in freq:
+                freq[x]=1
+            else:
+                freq[x]+=1
+        ans=[]
+        for x in nums2:
+            if x in freq and freq[x]>0:
+                ans.append(x)
+                freq[x]-=1
+        return ans
+        
