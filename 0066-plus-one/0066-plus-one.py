da5c0806c5eb1363e981a06e9e140cbf1193639d
upagnaduba <151008060+upagnaduba@users.noreply.github.com>
2024-09-23 10:28:56 +0530

    Time: 21 ms (16.27%), Space: 11.7 MB (35.99%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def plusOne(self, digits):
+        carry=1
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]+carry>9:
+                digits[i]=(digits[i]+carry)%10
+                carry=1
+            else:
+                digits[i]=digits[i]+carry
+                carry=0
+        if carry == 1:
+            digits.insert(0,1)
+        return digits
