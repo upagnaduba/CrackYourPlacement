19c8ff9f30787f60eae8f043c16477b80072aa11
upagnaduba <151008060+upagnaduba@users.noreply.github.com>
2025-02-18 11:39:56 +0530

    Time: 979 ms (74.79%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,1 @@
+select s.user_id, round(sum(case when action='confirmed'then 1 else 0 end)/count(*),2) as confirmation_rate from Signups S left join Confirmations c on s.user_id=c.user_id group by s.user_id
